766037934704ce2e505223f67e181350daccbeb4
Ajay0916 <Ajay0916@users.noreply.github.com>
2026-08-22 09:51:06 +0530

    fix: remove _restart from sites list (was showing as site button in Vj-wz)

--- routers/v1/sites_list_router.py
+++ routers/v1/sites_list_router.py
@@ -26,12 +26,6 @@ async def get_all_supported_sites():
                 "name": info["website"]._name,
             }
         )
-    # Special entry: restart t-API button
-    sites_list.append("_restart")
-    sites.append({
-        "site": "_restart",
-        "name": "Restart t-API",
-    })
     return error_handler(
         status_code=status.HTTP_200_OK,
         json_message={
